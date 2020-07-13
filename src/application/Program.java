963499c8a4562470041a0867229cddfa8157cd74
laurati <lauraecomarquez@gmail.com>
2020-07-13 09:16:06 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -30,6 +30,12 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("=== Teste 4: seller insert ===");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0,  department);
+		sellerDao.insert(newSeller);
+		System.out.println("Novo vendedor, id = " + newSeller.getId());
+		
 	}
 
 }
